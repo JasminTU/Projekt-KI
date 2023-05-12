c5e7a7eee3064c9e135c116df0dfd149d0eaa077
huju <hujudev@gmail.com>
2023-05-12 17:40:32 +0200

    add illegal pawn move test and change function name of fen function

--- tests.py
+++ tests.py
@@ -9,13 +9,15 @@ class TestChessBitboard(unittest.TestCase):
 
     def test_initial_board(self):
         self.bitboard.initialize_bitboards()
-        self.testboard.fen_to_bitboards("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+        self.testboard.load_from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
         self.assertEqual(self.bitboard.bitboards, self.testboard.bitboards)
 
-    def test_move_pawn(self):
+    def test_move_pawn_legal_1(self):
         self.bitboard.perform_move('e2e4')
-        self.testboard.fen_to_bitboards("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1")
+        self.testboard.load_from_fen("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1")
         self.assertEqual(self.bitboard.bitboards, self.testboard.bitboards)
+    def test_move_pawn_illegal_1(self):
+        self.assertRaises(Exception, self.bitboard.perform_move, 'e7e5')
 
 if __name__ == '__main__':
     unittest.main()
