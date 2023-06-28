55eb6ad747ee25f365464dcccf89e56b1a909034
Henri Evard <your.email@example.com>
2023-06-28 20:57:12 +0200

    implemented game state, pawn differentiation and piece square tables

--- ChessEngine.py
+++ ChessEngine.py
@@ -17,23 +17,16 @@ class ChessEngine():
         empty_squares = ~(board.bitboards[constants.WHITE] | board.bitboards[constants.BLACK])
         pawn_moves = []
 
-        white_pawn_bitboard = int(
-            "0b0000000000000000000000000000000000000000000000001111111100000000", 2
-        )
-        black_pawn_bitboard = int(
-            "0b0000000011111111000000000000000000000000000000000000000000000000", 2
-        )
-
         if board.current_player == constants.WHITE:
             one_step = (board.bitboards[constants.PAWN] & board.bitboards[constants.WHITE] & constants.NOT_TOP_EDGE) << 8 & empty_squares
-            two_steps = ((board.bitboards[constants.PAWN] & white_pawn_bitboard) << 16) & empty_squares
+            two_steps = one_step << 8 & empty_squares
             captures_left = (board.bitboards[constants.PAWN] & board.bitboards[constants.WHITE] & NOT_LEFT_EDGE & constants.NOT_TOP_EDGE) << 7 & board.bitboards[
                 constants.BLACK]
             captures_right = (board.bitboards[constants.PAWN] & board.bitboards[constants.WHITE] & NOT_RIGHT_EDGE & constants.NOT_TOP_EDGE) << 9 & board.bitboards[
                 constants.BLACK]
         else:
             one_step = (board.bitboards[constants.PAWN] & board.bitboards[constants.BLACK] & constants.NOT_BOTTOM_EDGE) >> 8 & empty_squares
-            two_steps = ((board.bitboards[constants.PAWN] & black_pawn_bitboard) >> 16) & empty_squares
+            two_steps = one_step >> 8 & empty_squares
             captures_left = (board.bitboards[constants.PAWN] & board.bitboards[constants.BLACK] & NOT_LEFT_EDGE & constants.NOT_BOTTOM_EDGE) >> 9 & board.bitboards[
                 constants.WHITE]
             captures_right = (board.bitboards[constants.PAWN] & board.bitboards[constants.BLACK] & NOT_RIGHT_EDGE & constants.NOT_BOTTOM_EDGE) >> 7 & board.bitboards[
@@ -249,7 +242,13 @@ class ChessEngine():
         board.board_history.append(copied_chessBoard.bitboards)
         # Increase pawn counter for draw by 50 moves
         board.pawn_not_moved_counter = 0 if (from_square & board.bitboards[constants.PAWN]) != 0 else board.pawn_not_moved_counter + 1
-
+        # Check for game phase
+        if board.opening_bitboard & from_square:
+            board.opening_bitboard ^= from_square
+            board.opening_count += 1
+        # Update the game phase after each move
+        board.set_game_phase()
+        
     @staticmethod
     def _convert_pawn(board, to_square):
         TOP_EDGE = constants.NOT_TOP_EDGE ^ constants.MAX_VALUE # invert not top edge
